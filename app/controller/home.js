82fd65e78b816e270030a4bcc8e248e43f5661cc
abzhangzhipeng <m15176142633@163.com>
2021-08-09 16:31:52 +0800

    写第一个api接口

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,14 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    ctx.body = 'hi, 微信';
+  }
+  async list() {
+    const { ctx } = this;
+    ctx.body = {
+    	"msg":"ok",
+	    "data":[1,2,3,4],
+    };
   }
 }
 
